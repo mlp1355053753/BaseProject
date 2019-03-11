9d45bd4098b2a974de692927c84fc1ab0615216b
1355053753@qq.com <MA1201luping>
2019-03-11 19:52:18 +0800

    删除无用文件 & 引入项目文件

---
+++ BaseProject/BaseProject/Commons/MLCommons.h
@@ -0,0 +1,13 @@
+//
+//  MLCommons.h
+//  BaseProject
+//
+//  Created by apple on 2019/3/11.
+//  Copyright © 2019 mlp. All rights reserved.
+//
+
+#ifndef MLCommons_h
+#define MLCommons_h
+
+
+#endif /* MLCommons_h */
